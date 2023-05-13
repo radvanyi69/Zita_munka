fc14bfe4eaa4a4f395b4f55ff9f6cf675fdaaa8c
radvanyi69 <132403279+radvanyi69@users.noreply.github.com>
2023-05-13 16:22:18 +0200

    2

---
+++ kivalaszto.cpp
@@ -0,0 +1,460 @@
+#include "graphics.hpp"
+#include "kivalaszto.hpp"
+#include <vector>
+#include <string>
+#include <cmath>
+#include <iostream>
+
+using namespace genv;
+
+// zezezez
+
+_lista::_lista(vector<string> elemek)
+{
+    items = elemek;
+    itemindex = 0;
+    lX = 0;
+    lY = 0;
+    height = 12;
+    width = 100;
+    topitemindex = 0;
+    maxitem = 5;
+    fontmeret = 12;
+    font = "LiberationSans-Regular.ttf";
+    _lenyitva = false;
+    sormagassag = gout.cascent()+gout.cdescent();
+    focus = false;
+    gorgetes = false;
+}
+
+int _lista::Get_lX()
+    {
+        return lX;
+    }
+
+void _lista::Set_lX(int x)
+    {
+        lX = x;
+    }
+
+int _lista::Get_lY()
+    {
+        return lY;
+    }
+
+void _lista::Set_lY(int y)
+    {
+        lY = y;
+    }
+
+int _lista::Get_height()
+    {
+        return height;
+    }
+
+void _lista::Set_height(int Height)
+    {
+        height = Height;
+    }
+
+int _lista::Get_width()
+    {
+        return width;
+    }
+
+void _lista::Set_width(int Width)
+    {
+        width = Width;
+    }
+
+void _lista::Set_Items(vector<string> elemek)
+    {
+        items = elemek;
+    }
+
+void _lista::Set_maxitem(int Maxitem)
+    {
+        // számoljuk ki a maxitem értékét a lista elemei alapján
+        maxitem = Maxitem;
+    }
+
+int _lista::Get_maxitem()
+    {
+        return maxitem;
+    }
+
+bool _lista::Get_lenyitva()
+    {
+        return _lenyitva;
+    }
+
+void _lista::Rajzol()
+    {
+        height = maxitem * sormagassag+4;
+        if (gorgetes)
+        {
+                gout << color(255, 255, 255) << move_to(lX, lY)
+             << box_to(lX+width, lY+height)
+             << color(100, 100, 100) << move_to(lX+1, lY+1)
+             << box_to(lX+width-2, lY+height-2);
+              _lenyitva = true;
+             gout << refresh;
+        }
+        else
+        {
+            if (!_lenyitva)
+            {
+                gout << color(255, 255, 255) << move_to(lX, lY)
+             << box_to(lX+width, lY+height)
+             << color(100, 100, 100) << move_to(lX+1, lY+1)
+             << box_to(lX+width-2, lY+height-2);
+              _lenyitva = true;
+             gout << refresh;
+            }
+            else
+            {
+              _lenyitva = false;
+               gout << color(0, 0, 0) << move_to(lX, lY)
+             << box_to(lX+width, lY+height);
+
+             gout << refresh;
+            }
+        }
+
+
+    }
+
+void _lista::intoItems()
+    {
+
+        int fh = 0;
+        if (items.size()-topitemindex  > maxitem)
+        {
+            fh = maxitem;
+        }
+        else
+        {
+            fh = items.size() - topitemindex;
+        }
+        if (!_lenyitva)
+        {
+            r=0;
+            g=0;
+            b=0;
+        }
+        else
+        {
+            r=255;
+            g=255;
+            b=255;
+        }
+        for (int i=topitemindex; i < topitemindex+fh; i++)
+        {
+                gout << color(r,g,b) << move_to(lX+2, lY+(i-topitemindex)*sormagassag+3) << text(items[i]);
+        }
+
+    }
+//****************************************************
+
+beviteli::beviteli(int x, int y):szin()
+{
+    X0 = x;
+    Y0 = y;
+    width = 150;
+    height = 20;
+    focused = false;
+}
+
+bool beviteli::Focused()
+{
+    return focused;
+}
+
+string beviteli::Gettext()
+{
+    return Text;
+}
+
+void beviteli::Settext(string t)
+{
+    Text = t;
+}
+
+void beviteli::SetWidth(int w)
+{
+    width = w;
+    rajzol();
+}
+
+void beviteli::SetHeight(int h)
+{
+    height = h;
+    rajzol();
+}
+
+void beviteli::torol()
+{
+    gout << move_to(X0+2, Y0+2) << color(0,0,0) << box(width, height);
+    gout << refresh;
+}
+
+void beviteli::rajzol()
+{
+    if (focused)
+    {
+        r = 255;
+        g = 200;
+        b = 0;
+    }
+    else
+    {
+        r = 220;
+        g = 220;
+        b = 220;
+    }
+
+
+    gout << move_to(0,0) << color(r, g, b) << move_to(X0, Y0)
+         << box_to(X0+width, Y0+height) ;
+}
+
+bool beviteli::rajtavan(int ex, int ey)
+{
+    int box1x = X0;
+    int box1y = Y0;
+    int box2x = X0+width;
+    int box2y = Y0+height;
+    bool rajta=false;
+    if(ex<=box2x && ex>=box1x && ey<=box2y && ey>=box1y)
+    {
+        rajta=true;
+    }
+    return rajta;
+}
+
+void beviteli::TextKiir()
+{
+    TextTorol();
+    gout << move_to(0,0) << color(0, 0, 0) << move_to(X0, Y0)
+         << text(Text) ;
+}
+
+void beviteli::TextTorol()
+{
+    rajzol();
+}
+
+void beviteli::handle(genv::event ev)
+{
+    if (ev.button == btn_left)
+    {
+        focused = rajtavan(ev.pos_x, ev.pos_y);
+        torol();
+        rajzol();
+    }
+    if (ev.keycode > 0 && focused)
+    {
+        Text = Text + ev.keyutf8;
+        TextKiir();
+    }
+    if ((ev.keycode == key_backspace) && focused)
+    {
+        string s = "";
+        for (int i = 0; i < Text.length()-1; i++)
+        {
+            s = s + Text[i];
+        }
+        Text = s;
+        TextTorol();
+        TextKiir();
+    }
+    if (ev.keycode == key_delete && focused)
+    {
+        Text = "";
+        TextTorol();
+    }
+}
+
+//****************************************************
+
+beviteliEX::beviteliEX(int x, int y, vector<string> elemek): _lista(elemek)
+    {
+        posX = x;
+        posY = y;
+       // list_items = elemek;
+        Text = "";
+        fontmeret = 12;
+        font = "LiberationSans-Regular.ttf";
+        bevheight = 15;
+        bevwidth = 100;
+        tok = 0;
+    }
+
+bool beviteliEX::Focused()
+    {
+      return focus;
+    }
+bool beviteliEX::Get_nyitva()
+    {
+      //  return Lista.Get_lenyitva();
+    }
+void beviteliEX::Set_list_items(vector<string> items)
+    {
+     //   list_items = items;
+    }
+void beviteliEX::rajzol()
+    {
+        r = 220;
+        g = 220;
+        b = 220;
+
+        gout << move_to(0,0) << color(r, g, b) << move_to(posX, posY)
+             << box_to(posX+bevwidth, posY+bevheight) <<
+             move_to(posX+bevwidth, posY) << color(r, 3*g/4, b) << box_to(posX+bevwidth+bevheight, posY+bevheight);
+
+        gout << color(0,0,0) << move_to(posX+bevwidth+bevheight/4, posY+bevheight/5) << line_to(posX+bevwidth+3*bevheight/4, posY+bevheight/5) <<
+            line_to(posX+bevwidth+bevheight/2, posY+4*bevheight/5) << line_to(posX+bevwidth+bevheight/4, posY+bevheight/5);
+
+        //listaRajzol();
+    }
+void beviteliEX::listaRajzol()
+    {
+        Set_lX(posX);
+        Set_lY(posY+bevheight);
+        Set_height(Get_maxitem()*bevheight);
+        Set_width(bevwidth);
+        Set_Items(items);
+
+        Rajzol();
+        intoItems();
+    }
+
+
+void beviteliEX::Set_bevwidth(int w)
+    {
+        bevwidth = w;
+    }
+void beviteliEX::Set_bevheigth(int h)
+    {
+        bevheight = h;
+    }
+void beviteliEX::TextKiir()
+    {
+        string s = Text;
+        gout << color(0,0,0);
+        gout << move_to(posX, posY);
+        gout << text(s);
+    }
+string beviteliEX::Get_Text()
+    {
+        return Text;
+    }
+void beviteliEX::TextTorol()
+    {
+       rajzol();
+    }
+int beviteliEX::rajtavan_listaelemen(int ex, int ey)
+    {
+        int hatar = 0;
+        int box1x = 0;
+        int box1y = 0;
+        int box2x = 0;
+        int box2y = 0;
+        itemindex = -1;
+        if (items.size() <= maxitem)
+        {
+            hatar = items.size();
+        }
+        else
+        {
+            hatar  = maxitem;
+        }
+        for (int i=0; i < hatar; i++)
+        {
+            box1x = lX;
+            box2x = lX+width;
+            box1y = lY+sormagassag*i;
+            box2y = lY+sormagassag*(i+1)+2;
+            if(ex<=box2x && ex>=box1x && ey<=box2y && ey>=box1y)
+            {
+                itemindex = i+topitemindex;
+                Text = items[itemindex];
+                TextTorol();
+                TextKiir();
+            }
+        }
+    }
+bool beviteliEX::rajtavan_lenyiton(int ex, int ey)
+    {
+        int box1x = posX+bevwidth;
+        int box1y = posY;
+        int box2x = posX+bevwidth+bevheight;
+        int box2y = posY+bevheight;
+        bool rajta=false;
+        if(ex<=box2x && ex>=box1x && ey<=box2y && ey>=box1y)
+        {
+            rajta=true;
+        }
+        return rajta;
+    }
+bool beviteliEX::rajtavanlistan(int ex,int ey)
+    {
+        int box1x = lX;
+        int box1y = lY;
+        int box2x = lX+width;
+        int box2y = lY+height;
+        bool rajta=false;
+        if(ex<=box2x && ex>=box1x && ey<=box2y && ey>=box1y)
+        {
+            rajta=true;
+        }
+        return rajta;
+    }
+
+void beviteliEX::handle(genv::event e)
+    {
+        if (rajtavan_lenyiton(e.pos_x, e.pos_y) && e.button == btn_left)
+        {
+            gorgetes = false;
+            listaRajzol();
+        }
+
+        if (e.button == btn_left)
+        {
+            gorgetes = false;
+            focus = rajtavanlistan(e.pos_x, e.pos_y) || rajtavan_lenyiton(e.pos_x, e.pos_y) ;
+            itemindex = rajtavan_listaelemen(e.pos_x, e.pos_y);
+
+        }
+        if (focus && e.button == btn_wheeldown)
+        {
+            gorgetes = true;
+            topitemindex++;
+
+            if (topitemindex > items.size()) topitemindex = items.size();
+            listaRajzol();
+
+        }
+        if (focus && e.button == btn_wheelup)
+        {
+            gorgetes = true;
+            topitemindex--;
+            if (topitemindex < 0) topitemindex = 0;
+            listaRajzol();
+        }
+//
+
+    }
+
+bool beviteliEX::rajtavan(int box1x, int box1y,int box2x, int box2y, int ex, int ey)
+{
+    bool rajta=false;
+    if(ex<=box2x && ex>=box1x && ey<=box2y && ey>=box1y)
+    {
+        rajta=true;
+    }
+    return rajta;
+}
+
+
+
---
+++ kivalaszto.hpp
@@ -0,0 +1,113 @@
+#ifndef KIVALASZTO_HPP_INCLUDED
+#define KIVALASZTO_HPP_INCLUDED
+#include "graphics.hpp"
+#include <vector>
+#include <string>
+#include <cmath>
+#include <iostream>
+using namespace std;
+using namespace genv;
+
+struct szin
+{
+protected:
+    int r, g, b;
+public:
+
+    int Get_Red();
+
+    void Set_Red(int red);
+    int Get_Green();
+
+    void Set_Green(int green);
+    int Get_Blue();
+    void Set_Blue(int blue);
+};
+
+class _lista:public szin
+{
+protected:
+    int lX, lY;
+    int height, width;
+    bool focus;
+    int topitemindex;
+    int maxitem;
+    int fontmeret;
+    string font;
+    bool _lenyitva;
+    vector<string> items;
+    int sormagassag;
+    bool gorgetes;
+public:
+    int itemindex;
+     _lista(vector<string> elemek);
+     int Get_lX();
+     void Set_lX(int x);
+     int Get_lY();
+     void Set_lY(int y);
+     int Get_height();
+     void Set_height(int Height);
+     int Get_width();
+     void Set_width(int Width);
+     void Set_Items(vector<string> elemek);
+     void Set_maxitem(int Maxitem);
+     int Get_maxitem();
+     bool Get_lenyitva();
+     void Rajzol();
+     void intoItems();
+};
+
+class beviteliEX: public _lista
+{
+private:
+
+    int posX, posY;
+    int bevheight, bevwidth;
+    string Text;
+    int fontmeret;
+    string font;
+    int tok;
+protected:
+
+public:
+    static beviteliEX& instance();
+    beviteliEX(int x, int y, vector<string> elemek);
+    bool Focused();
+    bool Get_nyitva();
+    void Set_list_items(vector<string> items);
+    void rajzol();
+    void listaRajzol();
+    void Set_bevwidth(int w);
+    void Set_bevheigth(int h);
+    void TextKiir();
+    string Get_Text();
+    void TextTorol();
+    int rajtavan_listaelemen(int ex, int ey);
+    bool rajtavan_lenyiton(int ex, int ey);
+    bool rajtavanlistan(int ex,int ey);
+    void handle(genv::event e);
+    bool rajtavan(int box1x, int box1y,int box2x, int box2y, int ex, int ey);
+};
+
+class beviteli: public szin
+{
+protected:
+    int X0, Y0, height, width;
+    string Text;
+    bool focused;
+public:
+   beviteli(int x, int y);
+   void Settext(string t);
+   string Gettext();
+   bool Focused();
+   void SetWidth(int w);
+   void SetHeight(int h);
+   void rajzol();
+   void torol();
+   bool rajtavan(int ex, int ey);
+   void TextKiir();
+   void TextTorol();
+   void handle(genv::event ev);
+};
+
+#endif // KIVALASZTO_HPP_INCLUDED
